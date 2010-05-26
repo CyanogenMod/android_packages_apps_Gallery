f8c2579f23c423ce191039af063f87e41586f8e2
Steve Kondik <shade@chemlab.org>
2010-05-26 15:27:24 -0400

    Add swipe gesture to change pictures.
    Change-Id: Idb6385038124ca27f16699f55a6ae4a6823e3b6a

--- src/com/android/camera/ViewImage.java
+++ src/com/android/camera/ViewImage.java
@@ -331,6 +331,29 @@ public class ViewImage extends Activity implements View.OnClickListener, ScaleGe
     private class MyGestureListener extends
             GestureDetector.SimpleOnGestureListener {
 
+        private static final int SWIPE_MIN_DISTANCE = 75;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    moveNextOrPrevious(1);
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    moveNextOrPrevious(-1);
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2,
                 float distanceX, float distanceY) {
@@ -372,7 +395,6 @@ public class ViewImage extends Activity implements View.OnClickListener, ScaleGe
     public boolean onScale(ScaleGestureDetector detector) {
         float scale = detector.getScaleFactor();
         float currentScale = mImageView.getScale();
-        Log.d("STRETCHY", "scale: " + scale + " currentScale: " + currentScale);
         
         if (currentScale < 0.7f && scale < 1.0f) {
             scale = 1.0f;
