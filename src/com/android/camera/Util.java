ab2a61a77d1b309c19e76e4370a8dae6910d74d0
Carl Shapiro <cshapiro@google.com>
2010-12-17 16:35:07 -0800

    Remove the only use of the inNativeAlloc option.
    Change-Id: I74516b0d7ec6018579723c6ce692f9c7e878f033

--- src/com/android/camera/Util.java
+++ src/com/android/camera/Util.java
@@ -431,8 +431,6 @@ public class Util {
 
     // Returns Options that set the puregeable flag for Bitmap decode.
     public static BitmapFactory.Options createNativeAllocOptions() {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inNativeAlloc = true;
-        return options;
+        return new BitmapFactory.Options();
     }
 }
